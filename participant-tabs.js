d634e3af7b705ccbb8a6a793eaf1a6830492b410
agwalkathon <prakashmali33@gmail.com>
2026-07-01 09:15:03 +0530

    Revert map tiles to CartoDB dark_all (Stadia requires API key); keep orange route color

--- participant-tabs.js
+++ participant-tabs.js
@@ -1628,7 +1628,7 @@ function initFeedMaps() {
         el._leafletMap = map;
         window._feedMaps.push(map);
 
-        L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
           maxZoom: 20
         }).addTo(map);
 
