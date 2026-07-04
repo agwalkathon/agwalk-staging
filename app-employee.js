e9f2032c1b710aa58f3f4bee42f155d8187f4193
agwalkathon <prakashmali33@gmail.com>
2026-07-04 10:58:43 +0530

    chore: sync and push app-employee.js (permissions correction)

--- app-employee.js
+++ app-employee.js
@@ -30,6 +30,11 @@ function tokenValid(t){
   try {
     var base64Url = t.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var pad = base64.length % 4;
+    if (pad) {
+      if (pad === 1) return false;
+      base64 += new Array(5 - pad).join('=');
+    }
     var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
       return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
